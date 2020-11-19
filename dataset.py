e485dc712fa87f3810cff5d43040b47bfd9293c8
javafalcon <javafalcon@163.com>
2020-11-19 22:23:51 +0800

    add capusle kears

--- dataset.py
+++ dataset.py
@@ -12,27 +12,31 @@ from tensorflow.keras.utils import Sequence
 import matplotlib
 
 num_negative, num_positive = 823576, 69750
-def generate_CAImage(num_iteration=82, num_rule=74):
-    with open('all_negative.fasta', 'r') as fn:
-        for i, record in enumerate(SeqIO.parse(fn, 'fasta')):
+
+def generate_CAImage(fastafile, save_dir, num_iteration=82, num_rule=74):
+    i = 0
+    with open(fastafile, 'r') as fn:
+        for record in SeqIO.parse(fn, 'fasta'):
             seq = str(record.seq)
+            if 'N' in seq:
+                continue
+            
             ca = CAImage(seq, num_iteration=num_iteration, num_rule=num_rule)
-            image_file = 'neg_CAImages/' + str(i) + ".jpg"
+            image_file = save_dir + str(i) + ".jpg"
             matplotlib.image.imsave(image_file, ca)
-            print("\rneg-{}".format(i), end='')
-    with open('all_positive.fasta', 'r') as fp:
-       for i, record in enumerate(SeqIO.parse(fp, 'fasta')):
-           seq = str(record.seq)
-           ca = CAImage(seq, num_iteration=num_iteration, num_rule=num_rule)
-           image_file = 'pos_CAImages/' + str(i) + ".jpg"
-           matplotlib.image.imsave(image_file, ca)
-           print("\rpos-{}".format(i), end='') 
+            print("\r{}-{}".format(fastafile, i), end='')
+            i += 1
+    
+           
 class SeqCAImageGenerator(Sequence):
-    def __init__(self, datas, labels, batch_size, shuffle=True):
+    def __init__(self, datas, labels, batch_size, 
+                 num_iteration=82, num_rule=82, shuffle=True):
         self.batch_size = batch_size
         self.datas = datas
         self.labels = labels
         self.indexes = np.arange(len(self.datas))
+        self.num_iteration = num_iteration
+        self.num_rule = num_rule
         self.shuffle = shuffle
     
     def __len__(self):
@@ -48,8 +52,9 @@ class SeqCAImageGenerator(Sequence):
         batch_labels = [self.labels[k] for k in batch_index]
         # 生成CA Image数据
         x = self.data_generation(batch_datas)
-        
-        return np.array(x), np.array(batch_labels)
+        x = np.array(x)
+        y = np.eye(2)[batch_labels]
+        return x.reshape((x.shape[0], x.shape[1], x.shape[2], 1)), y
     
     def on_epoch_end(self):
         # 在每一次epoch结束时，是否要进行一次随机，重新随机排列index
@@ -59,10 +64,48 @@ class SeqCAImageGenerator(Sequence):
     def data_generation(self, batch_datas):
         images = []
         for i, seq in enumerate(batch_datas):
-            image = CAImage(seq, num_iteration, num_rule)
+            image = CAImage(seq, self.num_iteration, self.num_rule)
             images.append(image)
         return images
     
-generate_CAImage()
-        
+
+# 统计数据集A、C、G、T分布
+# 结论如下：
+# 正类A、C、G、T出现频率：0.1479，0.3489，0.3576，0.1457
+# 负类A、C、G、T出现频率：0.2073, 0.3036, 0.2767, 0.2124
+# 全部A、C、G、T出现频率：0.2026, 0.3072, 0.2830, 0.2072, 所以按哈夫曼编码如下：
+# 在正类样本集中没有其它字符出现，在负类样本集中N出现34次
+def statFreq():
+    count = np.zeros((6,))
+    with open('all_negative.fasta','r') as fn:
+        for r in SeqIO.parse(fn,'fasta'):
+            for c in str(r.seq):
+                if c == 'A':
+                    count[0] += 1
+                elif c == 'C':
+                    count[1] += 1
+                elif c == 'G':
+                    count[2] += 1
+                elif c == 'T':
+                    count[3] += 1
+                elif c == 'N':
+                    count[4] += 1
+                else:
+                    count[5] += 1
+                    
+    with open('all_positive.fasta','r') as fn:
+        for r in SeqIO.parse(fn,'fasta'):
+            for c in str(r.seq):
+                if c == 'A':
+                    count[0] += 1
+                elif c == 'C':
+                    count[1] += 1
+                elif c == 'G':
+                    count[2] += 1
+                elif c == 'T':
+                    count[3] += 1
+                else:
+                    count[4] += 1
+                
+ 
 
